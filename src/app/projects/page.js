b633a01b4cc2bf2d1530dbbd389249725e1dfb07
juahhhhhhhhhh <juahhhhhhhhhh@gmail.com>
2024-04-11 00:35:49 +0900

    bugfix

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,31 +1,6 @@
 import { TOKEN, DATABASE_ID } from '../../../config/index';
 
-export default function Projects({ projects }) {
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
-        {projects && (
-          <>
-            <h1 className="text-4xl font-bold sm:text-6xl">
-              총 프로젝트 : <span className="pl-4 text-blue-500">{projects.results.length}</span>
-            </h1>
-            <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
-              {projects.results.map((aProject) => (
-                <div key={aProject.id} className="project-item">
-                  <h2>{aProject.title}</h2>
-                  <p>{aProject.description}</p>
-                  {/* 추가적인 데이터 표시 */}
-                </div>
-              ))}
-            </div>
-          </>
-        )}
-      </div>
-    </>
-  );
-}
-
-export async function loader() {
+async function getData() {
   const options = {
     method: 'POST',
     headers: {
@@ -37,7 +12,7 @@ export async function loader() {
     body: JSON.stringify({
       sorts: [
         {
-          property: '이름',
+          property: 'project-title',
           direction: 'ascending',
         },
       ],
@@ -46,60 +21,47 @@ export async function loader() {
   };
 
   const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
-  const projects = await res.json();
-  console.log('projects', projects);
-  return { projects };
+  const projectsResponse = await res.json();
+  console.log(
+    'projects',
+    projectsResponse.results.map((project) => project.properties['project-title'].title[0].plain_text)
+  );
+
+  return projectsResponse.results;
 }
 
-// app/page.js
+export default async function Projects() {
+  const results = await getData();
 
-// export default function Page({ projects }) {
-//   return (
-//     <>
-//       <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
-//         {projects && (
-//           <>
-//             <h1 className="text-4xl font-bold sm:text-6xl">
-//               총 프로젝트 : <span className="pl-4 text-blue-500">{projects.results.length}</span>
-//             </h1>
-//             <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
-//               {projects.results.map((aProject) => (
-//                 <div key={aProject.id} className="project-item">
-//                   <h2>{aProject.title}</h2>
-//                   <p>{aProject.description}</p>
-//                 </div>
-//               ))}
-//             </div>
-//           </>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-
-// export async function loader() {
-//   const notion = TOKEN;
-//   const databaseId = DATABASE_ID;
-
-//   const response = await notion.databases.query({
-//     database_id: databaseId,
-//     filter: {
-//       method: 'POST',
-//       headers: {
-//         Accept: 'application/json',
-//         'Notion-Version': '2022-06-28',
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${TOKEN}`,
-//       },
-//     },
-//     sorts: [
-//       // 정렬 옵션 설정
-//       {
-//         property: '이름',
-//         direction: 'ascending',
-//       },
-//     ],
-//   });
-
-//   return { projects: response };
-// }
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
+        {results && (
+          <>
+            <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
+              <h1 className="text-4xl font-bold sm:text-6xl">
+                총 프로젝트 : <span className="pl-4 text-blue-500">{results.length}</span>
+              </h1>
+              <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
+                {results.map((project) => (
+                  <div key={project.id} className="project-item">
+                    <h2>{project.properties['project-title'].title[0].plain_text}</h2>
+                    <p>
+                      프로젝트 링크: <a href={project.properties['project-link'].url}>{project.properties['project-link'].url}</a>
+                    </p>
+                    <p>
+                      프로젝트 기간: {project.properties['project-period'].date.start} - {project.properties['project-period'].date.end}
+                    </p>
+                    <p>프로젝트 기여도: {project.properties['project-contribution'].number * 100} %</p>
+                    <p>프로젝트 태그: {project.properties['project-tag'].multi_select.map((tag) => tag.name).join(', ')}</p>
+                    {/* 추가적인 데이터 표시 */}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
