14ff5a9cfda31bce564712d9946d320638674e43
juahhhhhhhhhh <juahhhhhhhhhh@gmail.com>
2024-04-12 01:35:29 +0900

    add - skills,home/ update - project layout

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -13,6 +13,7 @@ async function getData() {
       sorts: [
         {
           property: 'project-title',
+          property: 'project-sub-title',
           direction: 'ascending',
         },
       ],
@@ -26,33 +27,45 @@ async function getData() {
     'projects',
     projectsResponse.results.map((project) => project.properties['project-title'].title[0].plain_text)
   );
+  console.log(
+    'projects',
+    projectsResponse.results.map((project) => project.properties['project-sub-title'].rich_text[0].plain_text)
+  );
 
   return projectsResponse.results;
 }
 
-export default async function Projects() {
+export default async function Projects2() {
   const results = await getData();
-
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
-        {results && (
-          <>
-            <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
-              <h1 className="text-4xl font-bold sm:text-6xl">
-                총 프로젝트 : <span className="pl-4 text-blue-500">{results.length}</span>
-              </h1>
-              <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          {/* 타이틀 */}
+          {results && (
+            <>
+              <div className="flex flex-wrap w-full mb-20">
+                <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+                  <h2 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">프로젝트프로젝트프로젝트</h2>
+                  <div className="h-1 w-20 bg-amber-500 rounded"></div>
+                </div>
+                <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+                  프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트프로젝트 수는 입니다요
+                  {/* <span className="pl-4 text-blue-500">{results.length}</span> */}
+                </p>
+              </div>
+
+              <div className="flex flex-wrap -m-4">
+                {/* projectItem */}
+
                 {results.map((project) => (
-                  <>
-                    <ProjectItem key={project.id} data={project} />
-                  </>
+                  <ProjectItem key={project.id} data={project} />
                 ))}
               </div>
-            </div>
-          </>
-        )}
-      </div>
+            </>
+          )}
+        </div>
+      </section>
     </>
   );
 }
--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -1,10 +1,12 @@
 // projectItem.js
-import Image from 'next/legacy/image';
+// import Image from 'next/legacy/image';
 
 export default function ProjectItem(props) {
   const { data } = props;
 
   const title = data.properties['project-title'].title[0].plain_text;
+  const subTitle = data.properties['project-sub-title'].rich_text[0].plain_text;
+
   const link = data.properties['project-link'].url;
   const startDate = data.properties['project-period'].date.start;
   const endDate = data.properties['project-period'].date.end;
@@ -14,30 +16,52 @@ export default function ProjectItem(props) {
   const imgSrc = data.cover.file?.url || data.cover.external.url;
 
   return (
-    <div key={data.id} className="project-item">
-      {imgSrc && <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="100%" height="50%" layout="responsive" objectFit="cover" quality={100} />}
-      <div className="p-4 flex flex-col">
-        <h2 className="text-2xl font-bold">{title}</h2>
-        <p className='mt-4 text-xl"'>
-          프로젝트 링크: <a href={link}>{link}</a>
-        </p>
-        <p>
-          프로젝트 기간: {startDate} - {endDate}
-        </p>
-        <p>프로젝트 기여도: {contribution} %</p>
-        {/* <p className="flex items-start mt-2">
-          {tags}
-          <span className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"></span>
-        </p> */}
+    <>
+      {/* <div key={data.id} className="project-item">
+        {imgSrc && <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="100%" height="50%" layout="responsive" objectFit="cover" quality={100} />}
+        <div className="p-4 flex flex-col">
+          <h2 className="text-2xl font-bold">{title}</h2>
+          <p className='mt-4 text-xl"'>
+            프로젝트 링크: <a href={link}>{link}</a>
+          </p>
+          <p>
+            프로젝트 기간: {startDate} - {endDate}
+          </p>
+          <p>프로젝트 기여도: {contribution} %</p>
+          <div className="flex items-start mt-2">
+            {tags.map((tag) => (
+              <span className="px-2 py-1 mr-2 rounded-md bg-amber-200 dark:bg-amber-700 w-30" key={tag.id}>
+                {tag.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div> */}
 
-        <div className="flex items-start mt-2">
-          {tags.map((tag) => (
-            <span className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={tag.id}>
-              {tag.name}
-            </span>
-          ))}
+      <div key={data.id} className="xl:w-1/3 md:w-1/2 p-4">
+        <div className="bg-gray-100 dark:bg-neutral-800 p-6 rounded-lg">
+          {imgSrc && <img className="h-40 rounded w-full object-cover object-center mb-6" src={imgSrc} alt="content" />}
+          <h4 className="tracking-widest text-amber-500 text-xs font-medium title-font">{subTitle}</h4>
+          <h2 className="text-lg text-gray-900 font-medium title-font mt-2 mb-4">{title}</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <div className='mt-2 text-xl"'>
+            <a className="text-amber-400 inline-flex items-center md:mb-2 lg:mb-0" href={link}>
+              {link}
+            </a>
+          </div>
+          <div className="mt-2">
+            프로젝트 기간: {startDate} ~ {endDate}
+          </div>
+          <div className="mt-2">프로젝트 기여도: {contribution} %</div>
+          <div className="flex items-start mt-4">
+            {tags.map((tag) => (
+              <span className="px-2 py-1 mr-2 rounded-md bg-amber-500 w-30" key={tag.id}>
+                {tag.name}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
