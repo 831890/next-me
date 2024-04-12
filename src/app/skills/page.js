462efbb007e942c4d0c94830f0e403878eb8fb64
juahhhhhhhhhh <juahhhhhhhhhh@gmail.com>
2024-04-13 01:31:24 +0900

    ~~~~~

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -3,11 +3,11 @@ export default function Skills() {
     <>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flexWrap w-full mb-20 flex-col items-center text-center">
+          <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
             <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
           </div>
-          <div className="flex flexWrap -m-4">
+          <div className="flex flex-wrap -m-4">
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-amber-100 mb-4 pd-2">
